4bde9ce4c8a320406402d2d05c321cedd186ba4c
SkizardSel <Selinwk@gmail.com>
2023-11-19 15:17:39 +1100

    Main Menu Exists now

---
+++ Assets/Scripts/MenuScript.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour
+{
+	public GameObject pausePanel;
+	public bool IsPaused;
+
+	void Start()
+	{
+		pausePanel.SetActive(false);
+		pausePanel.SetActive (false);
+		Time.timeScale = 1f;
+		IsPaused = false;
+	}
+	public void ChangeSceneByIndex(int sceneId)
+	{
+	SceneManager.LoadScene(sceneId);
+	}
+
+   public void Egress()
+   {
+	 Application.Quit();
+		#if UNITY_EDITOR
+	 UnityEditor.EditorApplication.isPlaying = false;
+	    #endif
+   }
+   
+   private void Update()
+   {
+		if (Input.GetKeyDown(KeyCode.Escape)||Input.GetKeyDown(KeyCode.P))
+		{
+			if (IsPaused)
+			{
+				ResumeGame();
+			}
+			else
+			{
+				PauseGame();
+			}
+		}
+   }
+   private void PauseGame()
+   {
+		pausePanel.SetActive (true);
+		Time.timeScale = 0f;
+		IsPaused = true;
+   }
+   public void ResumeGame()
+   {
+		pausePanel.SetActive (false);
+		Time.timeScale = 1f;
+		IsPaused = false;
+   }
+}
