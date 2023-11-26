768925b04dcfa685b266ffd3e23c6c6e4765f6ae
ehrenholgersson <112737007+ehrenholgersson@users.noreply.github.com>
2023-11-26 17:46:27 +1100

    Audio too fast!!!

--- Assets/Scripts/Audio.cs
+++ Assets/Scripts/Audio.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class Audio : MonoBehaviour
 {
+    AudioSource _audio;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _audio = GetComponent<AudioSource>();
+        _audio.Play();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (GameControl.RemainingTime < 20)
+        {
+            _audio.pitch = 5 - (GameControl.RemainingTime / 5);
+        }
     }
 }
